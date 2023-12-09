1f9451321d35376e46e84df192638a7a10f54f4c
thiagorabaioli <tfrabaioli@outlook.com>
2023-12-09 19:01:03 +0000

    Associations class

--- src/main/java/tfr/dev/tfrDSCommerce/entities/OrderItem.java
+++ src/main/java/tfr/dev/tfrDSCommerce/entities/OrderItem.java
@@ -1,7 +1,11 @@
 package tfr.dev.tfrDSCommerce.entities;
 
 import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
 
+@Entity
+@Table(name="tb_order_item")
 public class OrderItem {
 
     @EmbeddedId
